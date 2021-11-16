75a1596074ae913d2e2f90b029fbbe27d852af70
Anton Pchelkin <clinic.death@gmail.com>
2021-11-16 18:24:37 +0300

    walk animation

--- game_plugin/src/player.rs
+++ game_plugin/src/player.rs
@@ -1,6 +1,16 @@
 use crate::{GameState, loading::TextureAssets};
 use bevy::prelude::*;
 
+pub enum Animation {
+    Stay,
+    Walk,
+    Jump,
+}
+
+pub struct PlayerAnim {
+    pub anim: Animation,
+    pub n_frames: usize,
+}
 
 pub struct PlayerPlugin;
 
@@ -33,15 +43,27 @@ fn spawn_player(
             transform: Transform::from_translation(Vec3::new(0.0, 0.0, 2.0)),
             ..Default::default()
         })
+        .insert(PlayerAnim{ anim: Animation::Stay, n_frames: 2})
+        .insert(Timer::from_seconds(0.3, true));
+
+
+    commands
+        .spawn_bundle(SpriteSheetBundle {
+            sprite: TextureAtlasSprite::new(0),
+            texture_atlas: textures.player_walk.clone(),
+            transform: Transform::from_translation(Vec3::new(-100.0, 0.0, 2.0)),
+            ..Default::default()
+        })
+        .insert(PlayerAnim{ anim: Animation::Walk, n_frames: 8})
         .insert(Timer::from_seconds(0.3, true));
     
 }
 
-fn anim_player(time: Res<Time>, mut query: Query<(&mut Timer, &mut TextureAtlasSprite)>) {
-    for (mut timer, mut sprite) in query.iter_mut() {
+fn anim_player(time: Res<Time>, mut query: Query<(&mut Timer, &mut TextureAtlasSprite, &PlayerAnim)>) {
+    for (mut timer, mut sprite, player_anim) in query.iter_mut() {
         timer.tick(time.delta());
         if timer.finished() {
-            sprite.index = ((sprite.index as usize + 1) % 2) as u32;
+            sprite.index = ((sprite.index as usize + 1) % player_anim.n_frames) as u32;
         }
     }
 }
