e9da43b1f089323c98de592876b4d9485682c1ee
Linnik9824 <Linnik9824@gmail.com>
2021-11-18 19:29:53 +0300

    changed movement

--- game_plugin/src/physics.rs
+++ game_plugin/src/physics.rs
@@ -1,11 +1,19 @@
 use crate::GameState;
 use bevy::prelude::*;
-use crate::consts::{ARENA_H, ARENA_W, PLAYER_TILE_SIZE};
 pub struct PhysicsPlugin;
 
 pub struct Velocity{
-  pub v: Vec2
+  pub vel: Vec2
 }
+
+pub struct IntendentMovement{
+  pub movement: Vec3
+}
+
+// pub struct Velocity{
+//   pub vel: Vec3
+// }
+
 
 pub struct Gravity(pub f32);
 pub struct AffectedByGravity;
@@ -27,32 +35,25 @@ fn gravity_system(
   mut velocities : Query <&mut Velocity, With<AffectedByGravity>>,
 ) {
   for mut velocity in  velocities.iter_mut() {
-    velocity.v.y -= gravity.0 * time.delta_seconds();
-    // if velocity.v.y>0.0 {
-    //   println!("{:}",velocity.v.y);
-    // }
-    
-    velocity.v.x = 150.0;
-    
-    velocity.v.y = velocity.v.y.clamp(            
-      0.5 * (-ARENA_H + PLAYER_TILE_SIZE),
-      2000.0
-    );
+    velocity.vel.y -= gravity.0 * time.delta_seconds();    
+    //velocity.vel.x -= gravity.0 * time.delta_seconds();
   }  
 }
 
 fn velocity_system(
+  mut commands: Commands,
   time: Res<Time>,
-  mut positions:Query<(&mut Transform, &mut Velocity)>)
-   //mut velocities:Query<&mut Velocity>) 
-   {
+  mut positions:Query<(Entity, &Velocity), With<AffectedByGravity>>
+  )  
+  {
+    
     let delta = time.delta_seconds();
-    for (mut position, mut velocity) in positions.iter_mut() {
-      position.translation.y = position.translation.y + velocity.v.y *delta; 
-
-      position.translation.y = position.translation.y.clamp(            
-        0.5 * (-ARENA_H + PLAYER_TILE_SIZE),
-        0.5 * (ARENA_H - PLAYER_TILE_SIZE),
+      for (entity, velocity) in positions.iter_mut() {
+        let movement = Vec3::new(
+          velocity.vel.x,
+          velocity.vel.y * delta,
+          0.,
       );
+      commands.entity(entity).insert(IntendentMovement{movement:movement});
+      }
   }
-}
--- game_plugin/src/player.rs
+++ game_plugin/src/player.rs
@@ -1,9 +1,12 @@
 use crate::actions::Actions;
 use crate::consts::{ARENA_H, ARENA_W, PLAYER_TILE_SIZE};
-use crate::loading::TextureAssets;
 use crate::GameState;
 use crate::physics::*;
 use bevy::prelude::*;
+use crate::loading::{TextureAssets};
+use  crate::map::*;
+use bevy::sprite::collide_aabb::collide;
+
 
 #[derive(Debug, PartialEq, Clone, Copy)]
 pub enum Animation {
@@ -17,6 +20,10 @@ pub struct PlayerAnim {
     pub n_frames: usize,
 }
 
+pub struct  FinaMovement{
+    movement: Vec3,
+}
+
 pub struct Player;
 pub struct PlayerPlugin;
 
@@ -27,8 +34,10 @@ impl Plugin for PlayerPlugin {
         )
         .add_system_set(
             SystemSet::on_update(GameState::Playing)
+                .with_system(register_movement.system())
                 .with_system(anim_player.system())
-                .with_system(move_player.system()),
+                .with_system(move_player.system())
+                .with_system(change_anim.system()), 
         );
     }
 }
@@ -47,8 +56,9 @@ fn spawn_player(mut commands: Commands, textures: Res<TextureAssets>) {
         })
         .insert(Timer::from_seconds(0.2, true))
         .insert(Player)
-        .insert(Velocity{v:Vec2::ZERO})
+        .insert(Velocity{vel:Vec2::ZERO})
         .insert(AffectedByGravity);
+
 }
 
 fn anim_player(
@@ -63,60 +73,112 @@ fn anim_player(
     }
 }
 
-fn move_player(
-    mut commands: Commands,
+fn register_movement(
     time: Res<Time>,
     actions: Res<Actions>,
-    textures: Res<TextureAssets>,
-    mut player_query: Query<(&mut Transform, &mut Velocity, With<Player>)>,
-    mut player_anim: Query<(Entity, &mut PlayerAnim, &mut TextureAtlasSprite)>,
-) {    
-
-    if actions.player_movement.is_none() {        
-        for (entity, mut anim, mut sprite) in player_anim.iter_mut() {
-            if anim.anim == Animation::Stay {
-                continue;
-            }
-            anim.anim = Animation::Stay;
-            anim.n_frames = 2;
-            sprite.index = 0;
-            commands.entity(entity).insert(textures.player_stay.clone());            
+    mut player_query: Query<(&mut Velocity), With<Player>>,
+){
+    
+    if actions.player_movement.is_none() { 
+        for (mut velocity)in player_query.iter_mut() {        
+            velocity.vel.x = 0.0;            
         }
         return;
-    }
-
-    for (mut player_transform, mut vel, p) in player_query.iter_mut() {
-
+    }    
+    for (mut velocity) in player_query.iter_mut() {        
+        velocity.vel.x = actions.player_movement.unwrap().x * 150.0 * time.delta_seconds();
         if actions.player_movement.unwrap().y != 0.0 {
-            println!("{:}",actions.player_movement.unwrap().y);
-            vel.v.y = 400.0;
-        }       
-
-        player_transform.translation.x += actions.player_movement.unwrap().x * 150.0 * time.delta_seconds();
-
-        player_transform.translation.x = player_transform.translation.x.clamp(
-            0.5 * (-ARENA_W + PLAYER_TILE_SIZE),
-            0.5 * (ARENA_W - PLAYER_TILE_SIZE),
-          );
+            velocity.vel.y = 450.0;
+        }    
     }
+}
 
-    for (entity, mut anim, mut sprite) in player_anim.iter_mut() {
-        sprite.flip_x = actions.player_movement.unwrap().x >= 0.0;
+fn change_anim(
+    mut commands: Commands,
+    textures: Res<TextureAssets>, 
+    mut player_anim: Query<(Entity, &mut PlayerAnim, &mut TextureAtlasSprite, &Velocity)>,
+){
+    for (entity,mut anim, mut sprite, velocity) in player_anim.iter_mut() {
 
-        if actions.player_movement.unwrap().y > 0.0 {
+        sprite.flip_x = velocity.vel.x >= 0.0;     
+
+        if velocity.vel.y > 0.0 {
             if anim.anim != Animation::Jump{
                 sprite.index = 0;
+            }else if anim.anim == Animation::Jump {
+                continue;
             }
             anim.anim = Animation::Jump;            
             anim.n_frames = 3;
             commands.entity(entity).insert(textures.player_jump.clone());
             continue;            
         }
-        if  actions.player_movement.unwrap().x != 0.0 {
+        else if  velocity.vel.x != 0.0 {
+            if anim.anim != Animation::Walk{
+                sprite.index = 0;
+            }else if anim.anim == Animation::Walk {
+                continue;
+            }
             anim.anim = Animation::Walk;
             anim.n_frames = 7;
             commands.entity(entity).insert(textures.player_walk.clone()); 
         }
+        else {
+            if anim.anim == Animation::Stay {
+                continue;
+            }
+            anim.anim = Animation::Stay;
+            anim.n_frames = 2;
+            sprite.index = 0;
+            commands.entity(entity).insert(textures.player_stay.clone());
+        }
     }
+}
 
+fn collision(
+    mut commands: Commands,
+    mut player_query: Query<(Entity, &Transform, &IntendentMovement), (With<Player>, Changed<IntendentMovement>)>,
+    mut tile_qeury: Query<(&Transform, &MapTile), With<MapTile>>,
+){
+    let tile_size = Vec2::new(32.0, 32.0);
+    let player_size = Vec2::new(64.0, 64.0);
+    for (player_entity, player_transform, intendent_movement) in player_query.iter() {
+        for (tile_transform, map_tile) in tile_qeury.iter() {
+            if map_tile.tiletype!=TileType::Background {
+
+                let temp_player_transform = player_transform.translation + intendent_movement.movement;
+
+                if collide(tile_transform.translation, tile_size,temp_player_transform, player_size,).is_some() {
+                    
+                    let final_movement = Vec3::new(
+                        0.0,
+                        0.0,
+                        0.,
+                    );
+                    commands.entity(player_entity).insert(FinaMovement{movement:final_movement});
+                }
+                // else {
+                //     commands.entity(player_entity).insert(FinaMovement{movement:intendent_movement.movement});
+                // }
+            }
+        }
+    }
+}
+
+fn move_player(   
+    mut player_query: Query<(&mut Transform, &IntendentMovement), (With<Player>, Changed<IntendentMovement>)>    
+) { 
+    for (mut player_transform, movement) in player_query.iter_mut() {
+        println!("{:}", movement.movement);
+        player_transform.translation += movement.movement;
+
+        player_transform.translation.x = player_transform.translation.x.clamp(
+            0.5 * (-ARENA_W + PLAYER_TILE_SIZE),
+            0.5 * (ARENA_W - PLAYER_TILE_SIZE),
+        );
+        player_transform.translation.y = player_transform.translation.y.clamp(
+            0.5 * (-ARENA_H + PLAYER_TILE_SIZE),
+            0.5 * (ARENA_H - PLAYER_TILE_SIZE),
+        );        
+    }
 }
